770fc72c69dfc5806c8fd0d8fc4a859f5653f50d
Rishueee <134083360+Rishueee@users.noreply.github.com>
2023-09-28 17:20:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Iteration 1: Making the play button in the index.html functional.
+const play = document.getElementById("play-button")
+play.onclick = () => {
+    location.href = "./game.html"
+}
+// Description: When the play button is clicked the game.html page should be opened
